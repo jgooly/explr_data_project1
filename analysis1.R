cdece74f95ee3343acbc945165f9b4641850fa20
Julian Ganguli <julianganguli@gmail.com>
2014-10-06 16:30:24 -0700

    first push

---
+++ analysis1.R
@@ -0,0 +1,12 @@
+setwd("~/Documents/github_repositories/exploratory_data_analysis")
+library(lubridate)
+data <- read.table('household_power_consumption.txt', header = TRUE, na.strings = '?', sep = ';')
+names(data) <- tolower(names(data))
+data$date <- dmy(data$date)
+new.data <- data[data$date == ymd(20070201) | data$date == ymd(20070202),]
+rm(data)
+
+## look for NA values in new.data
+sapply(new.data, FUN = function(x) {sum(is.na(x))}) ## there are 0 NA values in new.data
+
+
